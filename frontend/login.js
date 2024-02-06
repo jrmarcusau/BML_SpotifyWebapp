f8bcf6bc33a1c0c3029a6971d5afb3d0430e4c26
Marcus Au <mjau@usc.edu>
2024-02-06 01:22:26 -0800

    Adding everything else

--- frontend/login.js
+++ frontend/login.js
@@ -7,5 +7,11 @@ document.getElementById('btn_login').addEventListener('click', async (event) => 
 document.getElementById('btn_continue').addEventListener('click', async(event) => {
     event.preventDefault();
     console.log("continuing");
-    window.location.href = "/main"
+    window.location.href = "/main";
+})
+
+document.getElementById('btn_continue2').addEventListener('click', async(event) => {
+    event.preventDefault();
+    console.log("continuing 2");
+    window.location.href = "/alt";
 })
