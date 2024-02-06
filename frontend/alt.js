39abb1506d6e7f47209778bbb28a7e82876fcde5
Marcus Au <mjau@usc.edu>
2024-02-06 04:07:10 -0800

    Alt page visual done

---
+++ frontend/alt.js
@@ -0,0 +1,44 @@
+const { getSystemErrorMap } = require("util");
+
+function updateAllSliders(value) {
+    console.log("this chill");
+    // Update the output for the general slider
+    document.getElementById('generalSliderOutput').value = value;
+
+    // Update each individual slider
+    let sliders = document.querySelectorAll('.sync-slider');
+    sliders.forEach(slider => {
+        slider.value = value;
+        //slider.classList.add('slider2'); THIS WORKS
+
+        // Update the corresponding output for each slider
+        let outputId = slider.getAttribute('data-output');
+        if (outputId) {
+            document.getElementById(outputId).value = value;
+        }
+    });
+}
+
+function checkboxChanged(checkbox) {
+    console.log("hipeee");
+    var sliderId = checkbox.id.replace('use_', 'delta_');
+    var slider = document.getElementById(sliderId);
+
+    if (checkbox.checked) {
+        slider.classList.add('slider2');  // Change to gray when checked
+    } else {
+        slider.classList.remove("slider2");
+    }
+}
+
+
+window.onload = function() {
+    console.log("this also chill");
+    let initialValue = 0.15; // Default value
+    document.getElementById('general_slider').value = initialValue;
+    updateAllSliders(initialValue);
+};
+
+
+
+
